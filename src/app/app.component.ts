89a7a1176cc94a62c24675f94ed7b9c47b9e1dae
Martin Campagnoli <mcampagnoli@canto.com>
2019-07-30 15:42:42 +0200

    add random country functionality and set up basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ApplicationRef, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'nravel-angular';
+export class AppComponent implements OnInit {
+  title = 'app';
+  rc = false;
+  rcOn = 'Start';
+  rcOff = 'Stop';
+  render = true;
+  randomCountryString = this.rcOn;
+  randomCountry = <any>{};
+  randomRating$: Observable<any>;
+  subs: any;
+  myControl = new FormControl();
+  countries: any[];
+  filteredOptions: Observable<string[]>;
+
+
+  constructor(
+      //private router: Router,
+      private appRef: ApplicationRef,
+      private http: HttpClient,
+      public sanitizer: DomSanitizer
+    ) {
+  }
+
+  ngOnInit() {
+    this.randomRating$ = new Observable((subscriber) =>{
+      let randomInterval = () => Math.floor(Math.random() * 500) + 1;
+      const intervalId = setInterval(() => {
+        this.render = false;
+        subscriber.next(true);
+    }, 1000);
+    return function unsubscribe() {
+        clearInterval(intervalId);
+      };
+    });
+    this.loadCountries();
+  }
+
+  toogleRandomRating() {
+      !this.rc ? this.startRandomRating() : this.stopRandomRating();
+      this.rc = !this.rc;
+  }
+
+  renderBanner(r){
+      setTimeout(() =>{
+        this.render = r;
+      }, 0);
+  }
+
+  private startRandomRating(){
+      this.randomCountryString = this.rcOff;
+      this.subs = this.randomRating$.subscribe(r => this.renderBanner(r));
+  }
+
+  private stopRandomRating(){
+      this.randomCountryString = this.rcOn;
+      this.subs.unsubscribe();
+  }
+
+  loadCountries() {
+    let urlCountries = 'assets/countries.json';
+    let countries$ = this.http.get<any>(urlCountries);
+    countries$.subscribe(
+                response => {
+                        this.countries = response;
+                        this.loadRandomCountry(response as any[])
+                        this.getCountryInfo(this.randomCountry);
+                        this.filteredOptions = this.myControl.valueChanges
+                          .pipe(
+                            startWith(''),
+                            map(value => this._filter(value))
+                          );
+                      });
+  }
+
+  loadRandomCountry(countries: Array<Object>){
+      let randomIndex = Math.floor((Math.random() * countries.length) + 0);
+
+      this.randomCountry = countries[randomIndex];
+      this.randomCountry.mapUrl = this.generateUrlMap(this.randomCountry);
+  }
+
+  getCountryInfo(rc){
+    var url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&titles="+ rc.name + "&format=json&origin=*";
+    let info$ = this.http.get(url);
+    info$.subscribe(response => this.randomCountry.info = (<any>response).query.pages[Object.keys((<any>response).query.pages)[0]].extract);
+  }
+
+  generateUrlMap(rc){
+  		return "https://www.google.com/maps/embed/v1/view?zoom=4&center=" + rc.latlng[0] + "," + rc.latlng[1] + "&key=AIzaSyB0iePXe1BeVIOJHifld241qJoTzXT085Q";
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.countries.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+       BrowserAnimationsModule,
+       HttpClientModule,
+       MatToolbarModule,
+       MatTabsModule,
+       MatListModule,
+       MatButtonModule,
+       MatAutocompleteModule,
+       FormsModule,
+       ReactiveFormsModule,
+       MatFormFieldModule,
+       MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
